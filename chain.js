2805e0c22552195bf04e678cda46553cc80c87c1
ShashwatRajput <75117913+ShashwatRajput@users.noreply.github.com>
2020-12-28 12:58:40 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,16 @@
+class Chain{
+    constructor(bodyA, bodyB){
+        var options = {
+            bodyA : bodyA,
+            bodyB : bodyB,
+            length : 20,
+            stiffness : 0.9
+        }
+        this.chain = Matter.Constraint.create(options);
+        World.add(world, this.chain);
+    }
+    display(){
+        strokeWeight(10);
+        line(this.chain.bodyA.position.x, this.chain.bodyA.position.y, this.chain.bodyB.position.x, this.chain.bodyB.position.y);
+    }
+}
